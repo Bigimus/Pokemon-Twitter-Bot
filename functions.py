a4a9f653fc50b5749dc98a6bb1ba38746125ae92
Brian Hockenjos <brianhockenjos@brians-air.myfiosgateway.com>
2023-01-20 13:00:08 -0500

    Added main method to main.py, added functions.py

---
+++ functions.py
@@ -0,0 +1,88 @@
+import json
+
+FILE_PATH = "/Users/brianhockenjos/Desktop/Python_Projects/Global_Variables/Tokens.json"
+API_TOKEN = ""
+API_SECRET_TOKEN = ""
+BEARER_TOKEN = ""
+CLIENT_ID = ""
+CLIENT_SECRET_ID = ""
+ACCESS_TOKEN = ""
+ACCESS_SECRET_TOKEN = ""
+
+def getData():
+    with open (FILE_PATH, "r") as file:
+        temp_data = json.load(file)  
+    return temp_data
+
+def setAPI(token):
+    global API_TOKEN
+    API_TOKEN = token
+    return API_TOKEN
+
+def setAPISecret(token):
+    global API_SECRET_TOKEN
+    API_SECRET_TOKEN = token
+    return API_SECRET_TOKEN
+
+def setBearer(token):
+    global BEARER_TOKEN
+    BEARER_TOKEN = token
+    return BEARER_TOKEN
+
+def setClient(ID):
+    global CLIENT_ID
+    CLIENT_ID = ID
+    return CLIENT_ID
+
+def setClientSecret(ID):
+    global CLIENT_SECRET_ID
+    CLIENT_SECRET_ID = ID
+    return CLIENT_SECRET_ID
+
+def setAccess(token):
+    global ACCESS_TOKEN
+    ACCESS_TOKEN = token
+    return ACCESS_TOKEN
+
+def setAccessSecret(token):
+    global ACCESS_SECRET_TOKEN
+    ACCESS_SECRET_TOKEN = token
+    return ACCESS_SECRET_TOKEN
+
+def setTokens():
+    temp_data = getData()
+    setAPI(temp_data["ADMIN"]["API_TOKEN"])
+    setAPISecret(temp_data["ADMIN"]["API_SECRET_TOKEN"])
+    setBearer(temp_data["ADMIN"]["BEARER_TOKEN"])
+    setClient(temp_data["APP1"]["CLIENT_ID"])
+    setClientSecret(temp_data["APP1"]["CLIENT_SECRET_ID"])
+    setAccess(temp_data["APP1"]["ACCESS_TOKEN"])
+    setAccessSecret(temp_data["APP1"]["ACCESS_SECRET_TOKEN"])
+    
+def getAPI():
+    global API_TOKEN
+    return API_TOKEN
+    
+def getAPISecret():
+    global API_SECRET_TOKEN
+    return API_SECRET_TOKEN
+
+def getBEARER():
+    global BEARER_TOKEN
+    return BEARER_TOKEN
+
+def getClient():
+    global CLIENT_ID
+    return CLIENT_ID
+
+def getClientSecret():
+    global CLIENT_SECRET_ID
+    return CLIENT_SECRET_ID
+
+def getAccess():
+    global ACCESS_TOKEN
+    return ACCESS_TOKEN
+
+def getAccessSecret():
+    global ACCESS_SECRET_TOKEN
+    return ACCESS_SECRET_TOKEN
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import functions as func
 """
     ||===============================||
     ||      Pokemon Twitter Bot      ||
@@ -6,5 +6,11 @@
 
     This bot will pull and imagine and data of a pokemon from the Pokemon API,
     It'll then post a tweet containing the picture and the information about the Pokemon.
-    
+    consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 """
+
+if __name__ == "__main__":
+    func.setTokens()
