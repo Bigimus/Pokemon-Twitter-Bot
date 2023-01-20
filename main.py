50243c9bf369b2d6d9ed0e77930ed2b195453fd6
Brian Hockenjos <brianhockenjos@brians-air.myfiosgateway.com>
2023-01-20 00:43:09 -0500

    Created main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+"""
+    ||===============================||
+    ||      Pokemon Twitter Bot      ||
+    ||===============================||
+
+    This bot will pull and imagine and data of a pokemon from the Pokemon API,
+    It'll then post a tweet containing the picture and the information about the Pokemon.
+    
+"""
